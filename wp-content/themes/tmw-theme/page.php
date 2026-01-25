d697dcb85beac8af38c0663f137725f12fdfe8e4
eldridgedall0 <eldridge.dallo@hotmail.com>
2026-01-25 17:50:10 -0600

    Update page.php

--- wp-content/themes/tmw-theme/page.php
+++ wp-content/themes/tmw-theme/page.php
@@ -1,57 +1,32 @@
 <?php
 /**
- * Template Name: Default Page
- * Template for all standard pages including Simple Membership pages
+ * Default Page Template
  *
  * @package tmw-theme
  */
 
-if (!defined('ABSPATH')) {
-    exit;
-}
-
 get_header();
 ?>
 
-<main id="main-content" class="site-main tmw-page-content">
-    <?php
-    while (have_posts()) :
-        the_post();
-        ?>
-        <article id="post-<?php the_ID(); ?>" <?php post_class('tmw-page'); ?>>
-            
-            <?php if (get_the_title()) : ?>
-                <header class="entry-header">
-                    <h1 class="entry-title"><?php the_title(); ?></h1>
-                </header>
+<div class="tmw-page">
+    <div class="tmw-page-content">
+        
+        <?php while (have_posts()) : the_post(); ?>
+        
+        <header class="tmw-page-header">
+            <h1><?php the_title(); ?></h1>
+            <?php if (has_excerpt()) : ?>
+            <p class="page-description"><?php echo get_the_excerpt(); ?></p>
             <?php endif; ?>
+        </header>
 
-            <div class="entry-content">
-                <?php
-                // Output the page content (including Simple Membership shortcodes)
-                the_content();
+        <div class="tmw-page-body">
+            <?php the_content(); ?>
+        </div>
 
-                // Page pagination for long pages
-                wp_link_pages(array(
-                    'before' => '<div class="page-links">' . esc_html__('Pages:', 'flavor-starter-flavor'),
-                    'after'  => '</div>',
-                ));
-                ?>
-            </div>
+        <?php endwhile; ?>
 
-            <?php
-            // If comments are open or there's at least one comment, load the comment template
-            if (comments_open() || get_comments_number()) :
-                comments_template();
-            endif;
-            ?>
+    </div>
+</div>
 
-        </article>
-    <?php
-    endwhile;
-    ?>
-</main>
-
-<?php
-get_sidebar(); // Optional - remove if you don't want a sidebar
-get_footer();
+<?php get_footer(); ?>
